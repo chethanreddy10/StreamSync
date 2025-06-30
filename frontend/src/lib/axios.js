8131a1eba543d123882a25667828e27e3db4c896
chethanreddy10 <chethanreddy614@gmail.com>
2025-06-30 21:19:44 +0530

    vercel deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL="http://localhost:5001/api" ;
+const BASE_URL=`${import.meta.env.VITE_REACT_APP_API_URL}/api` ;
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // send cookies with the request
